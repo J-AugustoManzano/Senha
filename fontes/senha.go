259cce75337ac83f8884eface996aa2d0c0d731c
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-05-17 14:25:05 -0300

    Add files via upload

---
+++ fontes/senha.go
@@ -0,0 +1,36 @@
+// Teste para uso de senha
+// Programa didático, não pode ser
+// usado profissionalmente
+
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+
+  var T int = 3
+  var CORRETO bool = false
+  var SENHA int
+
+  for { // simulação "repeat"
+    fmt.Print("Entre a senha de acesso: ")
+    fmt.Scanln(&SENHA)
+    if SENHA == 123 {
+      CORRETO = true
+    } else {
+      T = T - 1
+      fmt.Println("Tentativas restantes:", T)
+    }
+    if (CORRETO || T == 0) { 
+      break
+    } // simulação "until"
+  }
+
+  if (CORRETO == true) {
+    fmt.Println("Acesso liberado")
+  } else {
+    fmt.Println("Acesso bloqueado")
+  }
+}
