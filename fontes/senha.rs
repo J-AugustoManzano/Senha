23b115316004e51a41d52e79a5aeb702e5e2fbec
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-05-17 15:34:51 -0300

    Add files via upload

---
+++ fontes/senha.rs
@@ -0,0 +1,35 @@
+// Teste para uso de senha
+// Programa didático, não pode ser
+// usado profissionalmente
+
+use std::io::{self, Write};
+
+fn senha() {
+
+    let mut T: i32 = 3;
+    let mut CORRETO: bool = false;
+    let mut SENHA: i32;
+
+    loop { // simulação "repeat"
+        print!("Entre a senha de acesso: ");
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        SENHA = input.trim().parse().unwrap();
+        if (SENHA == 123) {
+            CORRETO = true;
+        } else {
+            T = T - 1;
+            println!("Tentativas restantes: {}", T);
+        }
+        if (CORRETO == true || T == 0) {
+            break;
+        } // simulação "until"
+    }
+
+    if (CORRETO == true) {
+        println!("Acesso liberado");
+    } else {
+        println!("Acesso bloqueado");
+    }
+}
