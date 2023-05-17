56279add0b59d872efd004f9152619730613afaa
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-05-17 16:01:14 -0300

    Add files via upload

---
+++ fontes/senha.cs
@@ -0,0 +1,48 @@
+// Teste para uso de senha
+// Programa didático, não pode ser
+// usado profissionalmente
+
+using System;
+
+class Program
+{
+  static void Main()
+  {
+        
+    int T = 3;
+    bool CORRETO = false;
+    int SENHA;
+
+    do // simulação "repeat"
+    {
+        Console.Write("Entre a senha de acesso: ");
+        string input = Console.ReadLine();
+        if (int.TryParse(input, out SENHA))
+        {
+            if (SENHA == 123)
+            {
+              CORRETO = true;
+            }
+            else
+            {
+              T = T - 1;
+              Console.WriteLine("Tentativas restantes: " + T);
+            }
+        }
+        else
+        {
+          Console.WriteLine("Entrada inválida! Tente novamente.");
+        }
+    }
+    while (!CORRETO == true || T == 0); // simulação "until"
+    if (CORRETO == true)
+    {
+      Console.WriteLine("Acesso liberado");
+    }
+    else
+    {
+      Console.WriteLine("Acesso bloqueado");
+    }
+      
+  }
+}
