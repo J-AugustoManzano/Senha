99783e6171169690766447d7fee12a15ab443ea7
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-05-17 15:52:39 -0300

    Add files via upload

---
+++ fontes/senha.swift
@@ -0,0 +1,29 @@
+// Teste para uso de senha
+// Programa didático, não pode ser
+// usado profissionalmente
+
+import Foundation
+
+var T: Int = 3
+var CORRETO: Bool = false
+var SENHA: Int
+
+repeat {
+    print("Entre a senha de acesso: ", terminator: "")
+    guard let input = readLine(), let SENHA = Int(input) else {
+        continue
+    }
+
+    if (SENHA == 123) {
+        CORRETO = true
+    } else {
+        T = T - 1
+        print("Tentativas restantes: \(T)")
+    }
+} while !(CORRETO == true || T == 0) // simulação "until"
+
+if (CORRETO == true) {
+    print("Acesso liberado")
+} else {
+    print("Acesso bloqueado")
+}
