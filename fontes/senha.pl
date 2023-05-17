6072b2d75caadf945fc7b0e0c1b568d7695475dd
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-05-17 15:12:06 -0300

    Add files via upload

---
+++ fontes/senha.pl
@@ -0,0 +1,25 @@
+use strict;
+use warnings;
+
+my $T       = 3;
+my $CORRETO = 0;
+my $SENHA;
+
+do {
+    print "Entre a senha de acesso: ";
+    $SENHA = <STDIN>;
+    chomp $SENHA;
+
+    if ($SENHA == 123) {
+        $CORRETO = 1;
+    } else {
+        $T = $T - 1;
+        print "Tentativas restantes: $T\n";
+    }
+} until ($CORRETO == 1 || $T == 0);
+
+if ($CORRETO == 1) {
+    print "Acesso liberado\n";
+} elsif ($T == 0) {
+    print "Acesso bloqueado\n";
+}
