b636348ef5ec80b315a17ce66f65c55e4faa85e7
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-05-18 10:07:39 -0300

    Add files via upload

---
+++ fontes/senha.dart
@@ -0,0 +1,32 @@
+// Teste para uso de senha
+// Programa didático, não pode ser
+// usado profissionalmente
+//
+// Linguagem Dart
+
+import 'dart:io';
+
+void main() {
+    
+  int T = 3;
+  bool CORRETO = false;
+  int? SENHA;
+
+  do { // simulação "repeat"
+    stdout.write('Entre a senha de acesso: ');
+    String? input = stdin.readLineSync();
+    SENHA = input != null ? int.tryParse(input) : null;
+    if (SENHA == 123) {
+      CORRETO = true;
+    } else {
+      T = T - 1;
+      print('Tentativas restantes: $T');
+    }
+  }  while (!(CORRETO == true || T == 0)); // simulação "until"
+
+  if (CORRETO == true) {
+    print('Acesso liberado');
+  } else {
+    print('Acesso bloqueado');
+  }
+}
