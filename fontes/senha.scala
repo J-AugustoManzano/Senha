529c7fa315beb6d4537a47000ec9197f64eaf783
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-05-18 11:33:07 -0300

    Add files via upload

---
+++ fontes/senha.scala
@@ -0,0 +1,31 @@
+// Teste para uso de senha
+// Programa didático, não pode ser
+// usado profissionalmente
+//
+// Linguagem Scala
+
+object Main {
+  def main(args: Array[String]): Unit = {
+      
+    var T: Int = 3
+    var CORRETO: Boolean = false
+    var SENHA: Int = 0
+
+    do { // simulação "repeat"
+      println("Entre a senha de acesso: ")
+      SENHA = scala.io.StdIn.readInt()
+      if (SENHA == 123) {
+        CORRETO = true
+      } else {
+        T = T - 1
+        println("Tentativas restantes: $T")
+      }
+    } while (!(CORRETO == true || T == 0)) // simulação "until"
+
+    if (CORRETO == true) {
+      println("Acesso liberado")
+    } else {
+      println("Acesso bloqueado")
+    }
+  }
+}
