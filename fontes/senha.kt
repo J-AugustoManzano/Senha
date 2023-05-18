119eecb8630f7a0f686837f5961223a13d417498
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-05-18 10:51:32 -0300

    Add files via upload

---
+++ fontes/senha.kt
@@ -0,0 +1,29 @@
+// Teste para uso de senha
+// Programa didático, não pode ser
+// usado profissionalmente
+//
+// Linguagem Kotlin
+
+fun main() {
+    
+    var SENHA: Int
+    var T = 3
+    var CORRETO = false
+
+    do { // simulação "repeat"
+        print("Entre a senha de acesso: ")
+        SENHA = readLine()?.toIntOrNull() ?: 0
+        if (SENHA == 123) {
+            CORRETO = true
+        } else {
+            T = T - 1
+            println("Tentativas restantes: $T")
+        }
+    } while (!(CORRETO || T == 0)) // simulação "until"
+
+    if (CORRETO) {
+        println("Acesso liberado")
+    } else {
+        println("Acesso bloqueado")
+    }
+}
