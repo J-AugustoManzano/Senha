5c9813c1f631f5521338a5b0422fd048930bd90d
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-05-19 00:26:31 -0300

    Update senha.swift

--- fontes/senha.swift
+++ fontes/senha.swift
@@ -11,21 +11,20 @@ var CORRETO: Bool = false
 var SENHA: Int
 
 repeat {
-    print("Entre a senha de acesso: ", terminator: "")
-    guard let input = readLine(), let SENHA = Int(input) else {
-        continue
-    }
-
-    if (SENHA == 123) {
-        CORRETO = true
-    } else {
-        T = T - 1
-        print("Tentativas restantes: \(T)")
-    }
+  print("Entre a senha de acesso: ", terminator: "")
+  guard let input = readLine(), let SENHA = Int(input) else {
+      continue
+  }
+  if (SENHA == 123) {
+     CORRETO = true
+  } else {
+     T = T - 1
+     print("Tentativas restantes: \(T)")
+  }
 } while !(CORRETO == true || T == 0) // simulação "until"
 
 if (CORRETO == true) {
-    print("Acesso liberado")
+  print("Acesso liberado")
 } else {
-    print("Acesso bloqueado")
+  print("Acesso bloqueado")
 }
