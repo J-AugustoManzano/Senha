ce98a435f6b999ca767c4d4999c76265080918b9
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-05-19 00:28:39 -0300

    Update senha.rs

--- fontes/senha.rs
+++ fontes/senha.rs
@@ -8,31 +8,31 @@ use std::io::{self, Write};
 
 fn senha() {
 
-    let mut T: i32 = 3;
-    let mut CORRETO: bool = false;
-    let mut SENHA: i32;
+  let mut T: i32 = 3;
+  let mut CORRETO: bool = false;
+  let mut SENHA: i32;
 
-    loop { // simulação "repeat"
-        print!("Entre a senha de acesso: ");
-        io::stdout().flush().unwrap();
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        SENHA = input.trim().parse().unwrap();
-        if (SENHA == 123) {
-            CORRETO = true;
-        } else {
-            T = T - 1;
-            println!("Tentativas restantes: {}", T);
-        }
-        if (CORRETO == true || T == 0) {
-            break;
-        } // simulação "until"
-    }
-
-    if (CORRETO == true) {
-        println!("Acesso liberado");
+  loop { // simulação "repeat"
+    print!("Entre a senha de acesso: ");
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    SENHA = input.trim().parse().unwrap();
+    if (SENHA == 123) {
+        CORRETO = true;
     } else {
-        println!("Acesso bloqueado");
+        T = T - 1;
+        println!("Tentativas restantes: {}", T);
     }
+    if (CORRETO == true || T == 0) {
+      break;
+    } // simulação "until"
+  }
+
+  if (CORRETO == true) {
+     println!("Acesso liberado");
+  } else {
+     println!("Acesso bloqueado");
+  }
 
 }
