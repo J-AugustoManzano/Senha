0541425926094cd97126924365020d4d5690f997
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-05-19 00:31:22 -0300

    Update senha.kt

--- fontes/senha.kt
+++ fontes/senha.kt
@@ -6,25 +6,25 @@
 
 fun main() {
     
-    var SENHA: Int
-    var T = 3
-    var CORRETO = false
+  var SENHA: Int
+  var T = 3
+  var CORRETO = false
 
-    do { // simulação "repeat"
-        print("Entre a senha de acesso: ")
-        SENHA = readLine()?.toIntOrNull() ?: 0
-        if (SENHA == 123) {
-            CORRETO = true
-        } else {
-            T = T - 1
-            println("Tentativas restantes: $T")
-        }
-    } while (!(CORRETO || T == 0)) // simulação "until"
-
-    if (CORRETO) {
-        println("Acesso liberado")
+  do { // simulação "repeat"
+    print("Entre a senha de acesso: ")
+    SENHA = readLine()?.toIntOrNull() ?: 0
+    if (SENHA == 123) {
+        CORRETO = true
     } else {
-        println("Acesso bloqueado")
+        T = T - 1
+        println("Tentativas restantes: $T")
     }
+  } while (!(CORRETO || T == 0)) // simulação "until"
+
+  if (CORRETO == true) {
+      println("Acesso liberado")
+  } else {
+      println("Acesso bloqueado")
+  }
     
 }
